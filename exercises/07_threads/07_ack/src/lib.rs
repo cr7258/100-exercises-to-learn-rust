b974eaea65a24ef9b7b78ca1d9a0b03ee33ab4db
cr7258 <chengzw258@163.com>
2025-10-06 12:33:00 +0800

    rust learning

--- exercises/07_threads/07_ack/src/lib.rs
+++ exercises/07_threads/07_ack/src/lib.rs
@@ -4,10 +4,19 @@ use crate::store::TicketStore;
 pub mod data;
 pub mod store;
 
+use data::{Ticket, TicketDraft};
+use store::TicketId;
+
 // Refer to the tests to understand the expected schema.
 pub enum Command {
-    Insert { todo!() },
-    Get { todo!() }
+    Insert {
+        draft: TicketDraft,
+        response_sender: Sender<TicketId>,
+    },
+    Get {
+        id: TicketId,
+        response_sender: Sender<Option<Ticket>>,
+    }
 }
 
 pub fn launch() -> Sender<Command> {
@@ -21,13 +30,14 @@ pub fn server(receiver: Receiver<Command>) {
     let mut store = TicketStore::new();
     loop {
         match receiver.recv() {
-            Ok(Command::Insert {}) => {
-                todo!()
+            Ok(Command::Insert {draft, response_sender}) => {
+                let ticket_id = store.add_ticket(draft);
+                // Sned the response back
+                response_sender.send(ticket_id).unwrap();
             }
-            Ok(Command::Get {
-                todo!()
-            }) => {
-                todo!()
+            Ok(Command::Get {id, response_sender}) => {
+                let ticket = store.get(id).cloned();
+                response_sender.send(ticket).unwrap();
             }
             Err(_) => {
                 // There are no more senders, so we can safely break
